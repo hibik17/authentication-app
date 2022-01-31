004dc60789fbbeb77bf84bdb09072babb03afe46
hibik17 <hibiki@macbook>
2022-01-31 13:06:32 +0900

    70 done

--- src/components/molecules/MenuDrawer.tsx
+++ src/components/molecules/MenuDrawer.tsx
@@ -10,20 +10,30 @@ import React, { VFC, memo } from "react";
 type Props = {
   onClose: () => void;
   isOpen: boolean;
+  onClickHome: () => void;
+  onClickManagement: () => void;
+  onClickSetting: () => void;
 };
 
 const MenuDrawer: VFC<Props> = memo((props) => {
   // propsの展開
-  const { onClose, isOpen } = props;
+  const { onClose, isOpen, onClickHome, onClickManagement, onClickSetting } =
+    props;
 
   return (
     <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
       <DrawerOverlay>
         <DrawerContent>
           <DrawerBody p={0} bg="gray.100">
-            <Button w="100%">Top</Button>
-            <Button w="100%">ユーザー一覧</Button>
-            <Button w="100%">設定</Button>
+            <Button w="100%" onClick={onClickHome}>
+              Top
+            </Button>
+            <Button w="100%" onClick={onClickManagement}>
+              ユーザー一覧
+            </Button>
+            <Button w="100%" onClick={onClickSetting}>
+              設定
+            </Button>
           </DrawerBody>
         </DrawerContent>
       </DrawerOverlay>
--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Box, Flex, Heading, Link, useDisclosure } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
 
-import MenuIconButton from "../../atoms/button/MenuIconButton";
 import MenuDrawer from "../../molecules/MenuDrawer";
+import MenuIconButton from "../../atoms/button/MenuIconButton";
 
 export const Header = () => {
+  // drawerのstateを管理するモジュールの使用
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  // 画面遷移の関数の記載
+  const history = useHistory();
+
+  // homeへの遷移
+  const onClickHome = useCallback(() => history.push("/home"), [history]);
+
+  // user管理画面への遷移
+  const onClickManagement = useCallback(
+    () => history.push("/home/user_management"),
+    [history]
+  );
+
+  // 設定画面への遷移
+  const onClickSetting = useCallback(
+    () => history.push("/home/setting"),
+    [history]
+  );
+
   return (
     <>
       <Flex
@@ -17,7 +38,11 @@ export const Header = () => {
         padding={{ base: 3, md: 5 }}
       >
         <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
-          <Heading as="h1" font-size={{ base: "md", md: "lg" }}>
+          <Heading
+            as="a"
+            font-size={{ base: "md", md: "lg" }}
+            onClick={onClickHome}
+          >
             ユーザー管理アプリ
           </Heading>
         </Flex>
@@ -28,13 +53,19 @@ export const Header = () => {
           display={{ base: "none", md: "flex" }}
         >
           <Box pr={4}>
-            <Link>ユーザー一覧</Link>
+            <Link onClick={onClickManagement}>ユーザー一覧</Link>
           </Box>
-          <Link>設定</Link>
+          <Link onClick={onClickSetting}>設定</Link>
         </Flex>
         <MenuIconButton onOpen={onOpen} />
       </Flex>
-      <MenuDrawer isOpen={isOpen} onClose={onClose} />
+      <MenuDrawer
+        isOpen={isOpen}
+        onClose={onClose}
+        onClickHome={onClickHome}
+        onClickManagement={onClickManagement}
+        onClickSetting={onClickSetting}
+      />
     </>
   );
 };
